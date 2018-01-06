fcedc1b2f0947cd21fa3f13bd893b6fb2c05191c
Erazed <spam_man@hotmail.co.uk>
2018-01-06 21:02:17 +0000

    added input blur on enter (to minimise the keyboard)

--- js/calculators.js
+++ js/calculators.js
@@ -1,3 +1,9 @@
 $('nav').find('li').on('click', 'a', function() {
   $('.navbar-collapse.show').collapse('hide');
 });
+
+$('input').keyup(function(event) {
+    if (event.which === 13) {
+      $(this).blur();
+    }
+});
